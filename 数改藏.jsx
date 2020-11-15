0c37c75ce7e92a50213ef58f0a4cb065a180dd48
ginba <ginba@sina.com>
2020-11-15 21:03:02 +0800

    ID里把数字改阿拉伯数字改藏文数字的工具
    把要处理的区域反白，在反白区里的数字可以用这个script 来处理。

---
+++ 数改藏.jsx
@@ -0,0 +1,22 @@
+﻿var myDocument = app.activeDocument;
+app.findTextPreferences = NothingEnum.nothing;
+app.changeTextPreferences = NothingEnum.nothing;
+
+var alaboshuzi = new Array("0","1","2","3","4","5","6","7","8","9")
+var zangshuzi = new Array("༠","༡","༢","༣","༤","༥","༦","༧","༨","༩")
+for (n=0; n<10; n++){
+    app.findChangeTextOptions.caseSensitive = false;
+    app.findChangeTextOptions.includeFootnotes = false;
+    app.findChangeTextOptions.includeHiddenLayers = false;
+    app.findChangeTextOptions.includeLockedLayersForFind = false;
+    app.findChangeTextOptions.includeLockedStoriesForFind = false;
+    app.findChangeTextOptions.includeMasterPages = false;
+    app.findChangeTextOptions.wholeWord = false;
+    
+    app.findTextPreferences.findWhat = alaboshuzi[n];
+    app.changeTextPreferences.changeTo = zangshuzi[n];
+    myDocument.selection[0].changeText();
+    
+	app.findTextPreferences = NothingEnum.nothing;
+	app.changeTextPreferences = NothingEnum.nothing;
+}
